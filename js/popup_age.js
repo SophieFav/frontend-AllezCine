6e13e12db2bf28aa4fa2ab9c78cb36870427cb79
Soso <sophie.fav05@gmail.com>
2018-11-08 16:33:28 +0100

    updates0811

--- js/popup_age.js
+++ js/popup_age.js
@@ -1,6 +1,7 @@
  //ici est l'age minimum
+
+/*
         age_mini = 18;
-        
         var reponse = prompt("Ce site est réservé au "+age_mini+" ans et plus. Ecrivez votre année de naissance s.v.p",2000);
         
         mydate = new Date;
@@ -10,27 +11,20 @@
         age = annee - reponse;
         
         if (age>=age_mini){
-        alert("Ok, vous avez: "+age+" ans. Alors vous pouvez entrer")
+            return elmt;
+        //alert("Ok, vous avez: "+age+" ans. Alors vous pouvez entrer")
         }else{
         alert("Vous avez : "+age+" ans. Vous n'êtes pas autorisé à voir ce contenu.");
 
         //ici est redirigé la personne
         document.location.href='http://www.google.com'
         }
+        */
 
-        /*$( function() {
-            $( "#dialog-message" ).dialog({
-            resizable: false,
-            height: "auto",
-            width: 600,
-            modal: true,
-            buttons: {
-            "+ de 18": function() {
-            $( this ).dialog( "close" );
-            },
-            "- de 18": function() {
-            document.location.href="https://www.imdb.com/";
-            }
-            }
-            });
-            });*/
+        $(".yes").click(function(){
+            $("#popupage").hide();
+        });
+
+        $(".no").click(function(){
+            document.location.href='http://www.google.com';
+        });
