2e8499416ddb46ebbca5aa91e7a60c06597d6e82
Soso <sophie.fav05@gmail.com>
2018-11-12 11:14:17 +0100

    navscroll update

--- js/navscroll.js
+++ js/navscroll.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 520) {
-	    $(".navbartop").css("background" , "#111");
+	    $(".navbartop").css("background" , "#333");
 	  }
 
 	  else{
